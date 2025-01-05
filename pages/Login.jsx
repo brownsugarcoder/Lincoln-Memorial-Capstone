c40b82805e899073f451fa59966a4173a54a2960
brownsugarcoder <SHavon.d.howard@gmail.com>
2025-01-05 00:25:15 -0500

    login page update

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './login.css'
+import './login.css';
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -14,58 +14,61 @@ function Login() {
   };
 
   return (
-   <div className='login-wrapper'>
-    <div style={{ margin: '50px' }}>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <div style={{ marginBottom: '10px' }}>
-          <label htmlFor="email">Email:</label><br />
-          <input
-            id="email"
-            type="email"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            style={{ width: '200px' }}
-          />
-        </div>
-        
-        <div style={{ marginBottom: '10px' }}>
-          <label htmlFor="password">Password:</label><br />
-          <input
-            id="password"
-            type="password"
-            placeholder="Enter your password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            style={{ width: '200px' }}
-          />
-        </div>
-        
-        <div style={{ marginBottom: '10px' }}>
-          <label htmlFor="rememberMe">
-            <input
-              id="rememberMe"
-              type="checkbox"
-              checked={rememberMe}
-              onChange={() => setRememberMe(!rememberMe)}
-            />
-            {' '}Remember Me
-          </label>
-        </div>
-        
-        <button type="submit">Sign In</button>
-      </form>
+    <div className="background-container">
+      <div className="login-wrapper">
+        <div style={{ margin: '50px' }}>
+          <h2>Login</h2>
+          <form onSubmit={handleLogin}>
+            <div style={{ marginBottom: '10px' }}>
+              <label htmlFor="email">Email:</label><br />
+              <input
+                id="email"
+                type="email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                style={{ width: '200px' }}
+              />
+            </div>
 
-      <hr />
+            <div style={{ marginBottom: '10px' }}>
+              <label htmlFor="password">Password:</label><br />
+              <input
+                id="password"
+                type="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                style={{ width: '200px' }}
+              />
+            </div>
 
-      <p>Don’t have an account?</p>
-      <Link to="/register">Sign up</Link>
+            <div style={{ marginBottom: '10px' }}>
+              <label htmlFor="rememberMe">
+                <input
+                  id="rememberMe"
+                  type="checkbox"
+                  checked={rememberMe}
+                  onChange={() => setRememberMe(!rememberMe)}
+                />
+                {' '}Remember Me
+              </label>
+            </div>
+
+            <button type="submit">Sign In</button>
+          </form>
+
+          <hr />
+
+          <p>Don’t have an account?</p>
+          <Link to="/register">Sign up</Link>
+        </div>
+      </div>
     </div>
-    </div> 
   );
 }
 
 export default Login;
+
